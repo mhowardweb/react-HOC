51704eaad9e3eff25975f4b23e7bfcb785882531
mhowardweb <mhoward181@outlook.com>
2018-07-30 17:26:48 +0100

    Add middleware

--- README.md
+++ README.md
@@ -2,4 +2,8 @@ This project was bootstrapped with [Create React App]
 
 An app built whilst learning how to implement Higher Order Components in react apps.
 
+Redux middleware.
+Creating custom middleware.
+
+
 
--- src/Root.js
+++ src/Root.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import reduxPromise from 'redux-promise';
+import async from 'middlewares/async';
 import reducers  from 'reducers';
 
 // props are destructured to allow initialState to be used by test modules
@@ -11,7 +11,7 @@ export default ({ children, initialState = {} }) => {
   const store = createStore(
     reducers, 
     initialState, 
-    applyMiddleware(reduxPromise)
+    applyMiddleware(async)
   );
 
   return (
